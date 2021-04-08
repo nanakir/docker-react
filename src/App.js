8a8aafaf32366242f13313effacc9239126c7c4c
gharris <gharris@here.now>
2021-04-08 05:50:45 +0000

    updated app.js for aws

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          CHANGED <code>src/App.js</code> and save to reload.
+          ON AWS<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
